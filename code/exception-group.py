6e5afa05ac37e7607bb00238ed4a460a2302881d
Sadegh Yazdani <silverstar10@gmail.com>
2022-10-27 09:56:10 +0330

    feat: add `except*` for exception group

--- code/exception-group.py
+++ code/exception-group.py
@@ -3,6 +3,10 @@
 import asyncio
 
 
+async def write_to_db():
+    raise ConnectionError("not able to connect to database")
+
+
 async def connect_to_db():
     raise ConnectionError("Not connected")
 
@@ -18,6 +22,7 @@ async def prepare_request():
 async def do_request():
     res = await asyncio.gather(
         connect_to_db(),
+        write_to_db(),
         call_api(),
         prepare_request(),
         return_exceptions=True,
@@ -25,14 +30,29 @@ async def do_request():
     exceptions = [ex for ex in res if isinstance(ex, Exception)]
     if exceptions:
         raise ExceptionGroup("There were exceptions", exceptions)
-    
+
+
+# async def have_exception_group():
+#     await do_request()
+
 
 async def have_exception_group():
-    await do_request()
+    try:
+        await do_request()
+    except* ConnectionError as exg:  # `except*` is a New Syntax for exceptions
+        for ex in exg.exceptions:
+            print(f"We have problem in Connection. Error: {ex}")
+    except* ValueError as exg:
+        for ex in exg.exceptions:
+            print(f"There are some invalid values. Error: {ex}")
+    except* KeyError as exg:
+        for ex in exg.exceptions:
+            print(f"Could not find a required Key. Error: {ex}")
 
 
 async def main():
     await have_exception_group()
+
 
 if __name__ == "__main__":
     asyncio.run(main())
