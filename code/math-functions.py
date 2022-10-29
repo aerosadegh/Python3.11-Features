2eacf6ca3ce39d4585e83b3fd83ba3a4280ad0fe
Sadegh Yazdani <silverstar10@gmail.com>
2022-10-29 10:57:23 +0330

    style: change code style

--- code/math-functions.py
+++ code/math-functions.py
@@ -1,27 +1,30 @@
 """New Math Functions"""
 
 import math
-
-new_functions = {
-    "exp2": {
-        "old": (math.exp, 4),
-        "new": (math.exp2, 4),
-    },
-    "cbrt": {
-        "old": (math.sqrt, 4),
-        "new": (math.cbrt, 8),
-    },
-}
+from typing import Callable
 
 
-def print_item(title: str, func, value):
+def get_new_functions():
+    return {
+        "exp2": {
+            "old": (math.exp, 4),
+            "new": (math.exp2, 4),
+        },
+        "cbrt": {
+            "old": (math.sqrt, 4),
+            "new": (math.cbrt, 8),
+        },
+    }
+
+
+def print_item(title: str, func: Callable[[int], float], value: int):
     print(f"\t{title}")
     print(f"\t\t# {func.__doc__}")
     print(f"\t\t{func.__name__}({value}) = {func(value):.2f}")
 
 
 def main():
-    for item_name, func in new_functions.items():
+    for item_name, func in get_new_functions().items():
         print(f"New Function Name: {item_name!r}:")
         print_item("Like Previous:", *func["old"])
         print_item("Now we have", *func["new"])
