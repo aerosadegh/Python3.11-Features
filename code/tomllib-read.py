11343934daec03e6f2a2bbd5645accd2862a808d
Sadegh Yazdani <silverstar10@gmail.com>
2022-10-30 12:03:20 +0330

    style: improve structure

--- code/tomllib-read.py
+++ code/tomllib-read.py
@@ -3,11 +3,25 @@
 import tomllib
 from datetime import datetime
 from pathlib import Path
+from pprint import pprint
 
-toml_file_path = Path(__file__).parent.joinpath("extras/sample.toml")
 
-with open(toml_file_path, "rb") as toml_file:
-    content = tomllib.load(toml_file)
+def get_toml_content(toml_file_path: Path):
+    with open(toml_file_path, "rb") as toml_file:
+        content = tomllib.load(toml_file)
 
-content["process"]["start"] = datetime.fromisoformat(content["process"]["start"])
-print(content)
+    # process datetime string to Datetime object
+    content["process"]["start"] = datetime.fromisoformat(content["process"]["start"])
+    return content
+
+
+def main():
+    content = get_toml_content(
+        toml_file_path=Path(__file__).parent.joinpath("extras/sample.toml")
+    )
+    pprint(content)
+
+
+if __name__ == "__main__":
+    main()
+    print()
